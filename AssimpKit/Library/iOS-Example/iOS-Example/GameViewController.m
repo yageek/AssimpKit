1b204d86520db8126acb1e42d9a4ba891a6e7132
Dwayne Coussement <Dwayne@intivoto.com>
2018-07-12 07:59:35 -0500

    Introducing a very simple imagecache.

--- AssimpKit/Library/iOS-Example/iOS-Example/GameViewController.m
+++ AssimpKit/Library/iOS-Example/iOS-Example/GameViewController.m
@@ -45,7 +45,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     // Load the scene
     NSError *error = nil;
     SCNAssimpScene *scene =
-        [SCNScene assimpSceneWithURL:[NSURL URLWithString:self.modelFilePath]
+        [SCNScene assimpSceneWithURL:[NSURL fileURLWithPath:self.modelFilePath]
                     postProcessFlags:AssimpKit_Process_FlipUVs |
                                      AssimpKit_Process_Triangulate
                                error:&error];
@@ -55,7 +55,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     if (self.animFilePath)
     {
         SCNAssimpScene *animScene =
-            [SCNScene assimpSceneWithURL:[NSURL URLWithString:self.animFilePath]
+            [SCNScene assimpSceneWithURL:[NSURL fileURLWithPath:self.animFilePath]
                         postProcessFlags:AssimpKit_Process_FlipUVs |
                                          AssimpKit_Process_Triangulate
                                    error:&error];
