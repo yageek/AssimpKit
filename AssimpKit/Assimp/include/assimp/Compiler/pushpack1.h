af773f7ec710ad993f876037c7e38a74d04c7b9c
Lieven Dekeyser <lieven.dekeyser@showpad.com>
2020-07-23 14:39:20 +0200

    Update libassimp

--- AssimpKit/Assimp/include/assimp/Compiler/pushpack1.h
+++ AssimpKit/Assimp/include/assimp/Compiler/pushpack1.h
@@ -22,11 +22,11 @@
 #	error poppack1.h must be included after pushpack1.h
 #endif
 
-#if defined(_MSC_VER) ||  defined(__BORLANDC__) ||	defined (__BCPLUSPLUS__)
+#if (defined(_MSC_VER) && !defined(__clang__)) ||  defined(__BORLANDC__) ||	defined (__BCPLUSPLUS__)
 #	pragma pack(push,1)
 #	define PACK_STRUCT
-#elif defined( __GNUC__ )
-#	if defined(__clang__)
+#elif defined( __GNUC__ ) || defined(__clang__)
+#	if !defined(HOST_MINGW)
 #		define PACK_STRUCT	__attribute__((__packed__))
 #	else
 #		define PACK_STRUCT	__attribute__((gcc_struct, __packed__))
@@ -36,11 +36,8 @@
 #endif
 
 #if defined(_MSC_VER)
-
-// C4103: Packing was changed after the inclusion of the header, propably missing #pragma pop
+// C4103: Packing was changed after the inclusion of the header, probably missing #pragma pop
 #	pragma warning (disable : 4103) 
 #endif
 
 #define AI_PUSHPACK_IS_DEFINED
-
-
