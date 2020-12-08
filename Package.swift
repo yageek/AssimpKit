282f734bee8a59ae938cb140176eb187b96edaa0
Dave Henke <dave.henke@showpad.com>
2020-12-08 13:08:22 -0600

    Update to expose both frameworks

--- Package.swift
+++ Package.swift
@@ -6,24 +6,31 @@ let package = Package(
 	products: [
 		.library(
 			name: "AssimpKit",
-			targets: ["AssimpKitSPM"]
+			targets: ["AssimpKit"]
+		),
+		.library(
+			name: "AssimpKitDynamic",
+			type: .dynamic,
+			targets: ["AssimpKitDynamic"]
 		),
 	],
 	targets: [
-		// Needed to create a dynamic target that contains the statically linked AssimpKit
-		// Issue with App extensions embedding AssimpKit twice in the bundle and ITMS rejecting
-		// due to duplicate framework issues
-		// https://forums.swift.org/t/swift-packages-in-multiple-targets-results-in-this-will-result-in-duplication-of-library-code-errors/34892/47
-
-		.target(
-			name: "AssimpKitSPM",
-			dependencies: ["AssimpKit"],
-			path: "Sources"
-		),
 		.binaryTarget(
 			name: "AssimpKit",
 			url: "https://github.com/showpad/AssimpKit/releases/download/1.3.1/AssimpKit.xcframework.zip",
 			checksum: "68ed4708dcb19011d06bcd6bbfa47c3f53dc2af0beb236d546397f372baf7727"
+		),
+		
+		// Needed to create a dynamic target that contains the statically linked AssimpKit
+		// Issue with App extensions embedding AssimpKit twice in the bundle and ITMS rejecting
+		// due to duplicate framework issues
+		// https://forums.swift.org/t/swift-packages-in-multiple-targets-results-in-this-will-result-in-duplication-of-library-code-errors/34892/47
+		.target(
+			name: "AssimpKitDynamic",
+			dependencies: [
+				"AssimpKit",
+			],
+			path: "AssimpKitDynamic"
 		),
 	]
 )
